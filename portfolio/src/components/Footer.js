28cbb9bc29564d77c677f6b10477f99b8ea70785
ElginL <elginlee_2000@hotmail.com>
2022-12-08 10:47:01 +0800

    Add Footer.js

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,30 @@
+import styles from '../styles/components/Footer.module.css';
+import telegramImg from '../assets/telegram.png';
+import linkedinImg from '../assets/linkedin.png';
+import githubImg from '../assets/github.svg';
+
+const Footer = () => {
+    return (
+        <div className={styles["container"]}>
+            <h2 className={styles["contact-me-text"]}>
+                Contact Me!
+            </h2>
+            <div className={styles["links"]}>
+                <a href="https://www.linkedin.com/in/elgin-lee-81145b149/" target="_blank" className={styles["link"]}>
+                    <img src={linkedinImg} alt="LinkedIn" className={styles["contact-icon"]} />
+                </a>
+                <a href="https://t.me/ElginLee" target="_blank" className={styles["link"]}>
+                    <img src={telegramImg} alt="Telegram" className={styles["contact-icon"]} />
+                </a>
+                <a href="https://github.com/ElginL" target="_blank" className={styles["link"]}>
+                    <img src={githubImg} alt="Github" className={styles["contact-icon"]} />
+                </a>
+            </div>
+            <h2 className={styles["created-text"]}>
+                Created By Elgin
+            </h2>
+        </div>
+    );
+};
+
+export default Footer;
--- portfolio/src/pages/Landing.js
+++ portfolio/src/pages/Landing.js
@@ -3,6 +3,7 @@ import SelfIntroduction from '../components/SelfIntroduction';
 import styles from '../styles/pages/Landing.module.css';
 import Portfolio from '../components/Portfolio';
 import NavigationBar from '../components/NavigationBar';
+import Footer from '../components/Footer';
 
 const Landing = () => {
     return (
@@ -11,6 +12,7 @@ const Landing = () => {
             <SelfIntroduction />
             <AboutMe />
             <Portfolio />
+            <Footer />
         </div>
     )
 }
