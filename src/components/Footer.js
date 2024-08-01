370161a27777bda68f7d51044dc3d6a4f157226b
ElginL <elginlee_2000@hotmail.com>
2024-08-02 02:30:13 +0800

    Add contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,27 @@
 import styles from '../styles/components/Footer.module.css';
+import LinkedinImg from '../assets/linkedin.png';
+import TelegramImg from '../assets/telegram.png';
+import GithubImg from '../assets/TechLogos/github.svg';
 
 const Footer = () => {
     return (
         <div id="contact" className={styles["container"]}>
-            <h2 className={styles["created-text"]}>
-                Developed By Elgin Lee
-            </h2>
+            <div className={styles["content-container"]}>
+                <h2 className={styles["created-text"]}>
+                    Developed By Elgin Lee
+                </h2>
+                <div className={styles["links"]}>
+                    <a href="https://www.linkedin.com/in/elgin-lee-81145b149/" target="_blank" rel="noreferrer" className={styles["link"]}>
+                        <img src={LinkedinImg} alt="LinkedIn" className={styles["contact-icon"]} />
+                    </a>
+                    <a href="https://t.me/ElginLee" target="_blank" rel="noreferrer" className={styles["link"]}>
+                        <img src={TelegramImg} alt="Telegram" className={styles["contact-icon"]} />
+                    </a>
+                    <a href="https://github.com/ElginL" target="_blank" rel="noreferrer" className={styles["link"]}>
+                        <img src={GithubImg} alt="Github" className={styles["contact-icon"]} />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
